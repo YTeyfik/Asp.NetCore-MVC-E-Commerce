9d4f03812d8190f1c2f06001563850dce2b811da
YTeyfik <teyo2086@gmail.com>
2022-02-11 18:12:27 +0300

    Asp.net Core Empty project oluşturuldu.

---
+++ ShopApp.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //localhost:5000/home/index
+        public string Index()
+        {
+            return "home/index";
+        }
+        //localhost:5000/home/about
+        public string About()
+        {
+            return "home/about";
+        }
+    }
+}
---
+++ ShopApp.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //localhost/product/list
+        public string List()
+        {
+            return "product/list";
+        }
+        //localhost/product/details
+        public string Details()
+        {
+            return "product/list";
+        }
+    }
+}
